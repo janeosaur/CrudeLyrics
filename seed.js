189ea13ec68b773cc20e630e916dbc1a92b45d43
janeosaur <janewie@gmail.com>
2017-04-19 12:57:52 -0700

    added more seed data for lyrics

--- seed.js
+++ seed.js
@@ -302,6 +302,107 @@ lyricsList.push({
         writers: 'Addy'
       });
 
+lyricsList.push({
+        verse: "Ay, ay, ay\
+            Uh, ay what\
+            I don't need the lovin' nope\
+            So don't make this somethin'\
+            See, I'm nothin' like a girlfriend, no\
+            I'm not like someone I'm supposed to be\
+            And I just want some company, company\
+            And I just need some company, company\
+            I just need some company, company\
+            I just need some company, company\
+            Hey, yeah, shout out to my booty\
+            I'm straight up with this one boy, I know it\
+            I call it when I stop feelin' alone\
+            I want it and sometimes, see, I don't\
+            I don't need no problems with nobody\
+            I know how to get that, I don't follow me\
+            I just need a moment, fuck and leave\
+            You could say you had it all, no strings\
+            I don't need the lovin' nope\
+            So don't make this somethin'\
+            See, I'm nothin' like a girlfriend, no\
+            I'm not like someone I'm supposed to be\
+            And I just want some company, company\
+            And I just need some company, company\
+            I just need some company, company\
+            I just need some company, company\
+            Companions, but we keep it even lower\
+            I never ever been the type to ever just show up\
+            And you can't ever be the type of guy to just show up\
+            Treat it like a meet and greet and then we go up\
+            I don't need no problems with nobody\
+            I know how to get that, unfollow me\
+            I just need a moment, fuck and leave\
+            You could say you had it all, no strings\
+            Had it all\
+            You could say you had it all\
+            You could say you had it all\
+            You could say you had it all\
+            You could say you had it all\
+            You could say you had it all\
+            You could say you had it all\
+            All babe\
+            You're just my little side thing\
+            Just a little sweet fling\
+            Baby you don't have to buy things\
+            I just want the high, supply me\
+            I don't need the lovin' nope\
+            So don't make this somethin'\
+            See, I'm nothin' like a girlfriend, no\
+            I'm not like someone I'm supposed to be\
+            And I just want some company, company\
+            And I just need some company, company\
+            I just need some company, company\
+            I just need some company, company\
+            Da, da, da, I just need\
+            I just need some company, company\
+            I just need some company\
+            I",
+        language: 'English',
+        writers: 'Addy'
+    });
+
+lyricsList.push({
+        verse: "Lehani gangsta\
+            need a gangsta\
+            To love me better\
+            Than all the others do\
+            To always forgive me\
+            Ride or die with me\
+            That's just what gangsters do\
+            I'm fucked up, I'm black and blue\
+            I'm built for it, all the abuse\
+            I got secrets, that nobody, nobody knows\
+            I'm good on, that pussy shit\
+            I don't want, what I can get\
+            I want someone, with secrets\
+            That nobody, nobody, nobody knows\
+            I need a gangsta\
+            To love me better\
+            Than all the others do\
+            To always forgive me\
+            Ride or die with me\
+            That's just what gangsters do\
+            My freakness is on the loose\
+            And running, all over you\
+            Please take me to places, that nobody, nobody knows\
+            You got me hooked up on the feeling\
+            You got me hanging from the ceiling\
+            Got me up so high I'm barely breathing\
+            So don't let me, don't let me, don’t let me, don't let me go\
+            I need a gangsta\
+            To love me better\
+            Than all the others do\
+            To always forgive me\
+            Ride or die with me\
+            That's just what gangsters do",
+        language: 'English',
+        writers: 'Addy'
+    });
+
 db.Lyric.remove({}, function(err, lyrics) {
   db.Lyric.create(lyricsList, function(err, lyrics) {
   });
