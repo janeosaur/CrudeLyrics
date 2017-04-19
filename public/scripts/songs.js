1dcf19c695e4a970c9ef30c900ee35f46f69f227
addykumar27 <adarshna27@yahoo.com>
2017-04-19 13:42:47 -0700

    adding package.json and adding ajax in songs.js

--- public/scripts/songs.js
+++ public/scripts/songs.js
@@ -1,34 +1,67 @@
-$(document).ready(function() {
-  console.log('app.js loaded!');
+$(document). ready(function() {
+  console.log('song.js loaded');
 
-var express = require('express'),
-  bodyParser = require('body-parser');
+//requesting lyrics from lyrics page
+$.ajax ({
+  method: 'GET',
+  url:'/api/songs',
+  success: renderAllGenreSongs
+});
 
-var db = require('./models');
 
-var app = express();
+function renderAllGenreSongs (songs) {
+songs.forEach(function(song) {
+  renderSong(song);
 
-////////////////////
-//  MIDDLEWARE
-///////////////////
-
-app.use(express.static('public'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-function logRequestInfo(req, res, next){
-  console.log(`\nRECEIVED REQUEST : ${req.method} ${req.url}`);
-  console.log('query params:', req.query);
-  console.log('body:', req.body);
-  next();
+ });
 }
-app.use(logRequestInfo);
+ function renderSong(song) {
+  console.log('rendering all songs, song');
+  
+  genre.songs.html = genre.songs.map(renderSong).join("");
+
+  var songshtml =(
+<div class="container">
+  <div class="row">
+    <div class="col s8 offset-s1 genre" id="songsNameOne"><span class="flow-text"><a href="#">Song Name</a></span></div>
+    <ul>
+    <li><a.class="hover"></a></li>
+    <li><a class="waves-effect waves-purple btn" href="#">View Lyrics</a></li>
+    <li><a class="waves-effect waves-orange btn" href="#">Add Lyrics</a></li>
+    </ul>
+
+    <div class="col s8 offset-s1 genre" id="songsNameTwo"><span class="flow-text"><a href="#">Song Name</a></span></div>
+      <ul>
+      <li><a.class="hover"></a></li>
+      <li><a class="waves-effect waves-purple btn" href="#">View Lyrics</a></li>
+      <li><a class="waves-effect waves-orange btn" href="#">Add Lyrics</a></li>
+      </ul>
+
+    <div class="col s8 offset-s1 genre" id="songsNameThree"><span class="flow-text"><a href="#">Song Name</a></span></div>
+      <ul>
+      <li><a.class="hover"></a></li>
+      <li><a class="waves-effect waves-purple btn" href="#">View Lyrics</a></li>
+      <li><a class="waves-effect waves-orange btn" href="#">Add Lyrics</a></li>
+      </ul> 
+    </div>
+
+   $('#songs').prepend(songshtml);
+   
+   }	
 
 
-////////////////////
-//  ROUTES
-///////////////////
 
-app.get('/', function (req, res) {
-  res.sendFile('views/songs.html' , { root : __dirname});
-});  
+
+
+
+
+
+
+  	)
+
+
+
+
+
+
+ }
