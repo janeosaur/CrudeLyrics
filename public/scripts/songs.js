05b1c346527d50e0e8cbfae98d6e2402a97faf59
addykumar27 <adarshna27@yahoo.com>
2017-04-19 11:13:57 -0700

    adding songs.js

---
+++ public/scripts/songs.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  console.log('app.js loaded!');
+
+var express = require('express'),
+  bodyParser = require('body-parser');
+
+var db = require('./models');
+
+var app = express();
+
+////////////////////
+//  MIDDLEWARE
+///////////////////
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function logRequestInfo(req, res, next){
+  console.log(`\nRECEIVED REQUEST : ${req.method} ${req.url}`);
+  console.log('query params:', req.query);
+  console.log('body:', req.body);
+  next();
+}
+app.use(logRequestInfo);
+
+
+////////////////////
+//  ROUTES
+///////////////////
+
+app.get('/', function (req, res) {
+  res.sendFile('views/songs.html' , { root : __dirname});
+});  
