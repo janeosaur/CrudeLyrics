e6afb328e8f2af3d70f0b91601726c8eb883af0f
janeosaur <janewie@gmail.com>
2017-04-19 18:03:30 -0700

    edit view lyrics clickable

--- public/scripts/songs.js
+++ public/scripts/songs.js
@@ -10,20 +10,13 @@ $(document).ready(function() {
 
   $.ajax({
     method: 'GET',
-    url: '/api/genre/' + genre + '/songs', // server provides the genre specific song
+    url: '/api/genre/' + genre + '/songs', // server provides the genre specific songs!
     success: handleSuccess,
     error: handleError
   });
 
-  // // when user clicks on edit lyrics - adjust this so it applies to songs page.
-  // $('#edit').on('click', handleEditLyric);
-  //
-  // $('#delete').on('click', handleDeleteLyric);
-
-
-  function handleSuccess(res) { // renderAlbum
-    console.log(res); // logs array of song objects
-    $('.container').prepend(`<h1> ${genre} <h1>`);
+  function handleSuccess(res) {
+    $('.container').prepend(`<h1> ${genre} </h1>`);
     res.forEach(function(song) {
       console.log(`${song.name} ${song.artistName} ${song.releaseDate}`);
 
@@ -37,8 +30,8 @@ $(document).ready(function() {
                 </ul>
 
                 <div class='panel-footer valyrics'>
-                    <button class='btn btn-info view'>View Lyrics </button>
-                    <button class='btn btn-info add'>Add Lyrics </button>
+                    <button class='btn btn-info view-lyrics' onclick="ViewLyric()"> View Lyrics </button>
+                    <button class='btn btn-info' add-lyrics' onclick="AddLyric()""> Add Lyrics </button>
                 </div>
             </div>
             <!-- end one song -->
@@ -46,32 +39,19 @@ $(document).ready(function() {
           $('.row').append(songsHtml);
         });
       };
-
-
-
 });
-
-
-// function renderSong(song) {
-//   console.log(`${song.name}`);
-//   return `<span> ${song.name} ${song.artistName} </span>`
-// }
-
 
 function handleError(e) {
   console.log('uh oh');
 }
 
-
-// adjust below so it applies to songs page
-// function handleEditLyric(e) {
-//   console.log('edit lyric clicked');
-//   $('#editModal').modal();
-//   // does it make sense for users to edit parts of a lyric?
-//   // style the buttons so it's not touching side by side
-// }
-//
-// function handleDeleteLyric(e) {
-//   console.log('delete lyric clicked');
-//   $('#deleteModal').modal();
-// }
+// when user clicks on view lyrics
+function ViewLyric(e) {
+  console.log('view lyric clicked');
+  $('#viewModal').modal();
+}
+// when user clicks on add lyrics
+function AddLyric(e) {
+  console.log('add lyric clicked');
+  $('#addModal').modal();
+}
