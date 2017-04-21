09cd8eb995f6278bff34788a099fc255e00b0072
addykumar27 <adarshna27@yahoo.com>
2017-04-20 17:47:53 -0700

    adding update and delete function to song.js

--- public/scripts/songs.js
+++ public/scripts/songs.js
@@ -32,6 +32,8 @@ $(document).ready(function() {
 
 }); // end of doc on ready
 
+
+
 function handleError(e) {
   console.log('uh oh');
 }
@@ -49,8 +51,11 @@ function handleSuccess(res) {
 
                 <div class='panel-footer valyrics'>
 
-                    <button class='btn btn-info view-lyrics'> View Lyrics </button>
-                    <button class='btn btn-info' add-lyrics'> Add Lyrics </button>
+                    <button class='btn btn-info view-lyrics'>View Lyrics</button>
+                    <button class='btn btn-info add-lyrics'>Add Lyrics</button>
+                    <button class='btn btn-info edit-lyrics'>Edit Song</button>
+                    <button class='btn btn-info delete-lyrics'>Delete Song</button>
+
 
                 </div>
             </div>
@@ -72,37 +77,133 @@ function addSong(song) {
 
               <div class='panel-footer valyrics'>
                   <button class='btn btn-info view-lyrics'> View Lyrics </button>
-                  <button class='btn btn-info' add-lyrics'> Add Lyrics </button>
+                  <button class='btn btn-info add-lyrics'> Add Lyrics </button>
+
               </div>
           </div>
-          <!-- end one song -->
+
         `);
   $('div.songs-row').append(songsHtml);
   $('.view-lyrics').on('click', viewLyric);
 }
 
-
-
-
-// when user clicks on view lyrics
-
 function viewLyric(e) {
-  console.log('view lyric clicked');
-  var currentSong = $(this).closest('.song-output').data('name');
+ //clickedbutton();
+  //var currentSong = $(this).closest('.song-output').data('name');
   console.log(currentSong); // undefined
   }
 
-// when user clicks on add lyrics
-function addLyric(e) {
 
+function addLyric(e) {
+clickedbutton();
   console.log('add lyric clicked');
   $('.addModal').modal();
-  // modal doesn't open yet?
+  
 }
-
+function editLyric(e) {
+  clickedbutton();
+}
+function deleteLyric(e) {
+  clickedbutton();
+}
 function clickedbutton () {
  window.location = "/lyrics";
 }
+
+  //delete song when its delete button is clicked
+  //$('#').on('click', '.delete-song', handleDeleteSongClick);
+  //edit and save songs when edit button is clicked
+  //$('#').on('click', '.edit-song', handleSongEditClick);
+//};
+
+
+// function handleSongEditClick(e) {
+//   var $songsList = $(this).closest('.song');
+//   var songId = $songsList.data('song-id');
+ 
+
+//   //show the save and edit button after song as been edited.
+//   $songsList.find('.save-song'). toggleClass('hidden');
+//   $songsList.find('edit-song'). toggleClass('hidden');
+
+//   var songName = $songslist.find ('span.song-name'). text();
+//   $songslist.find('span.song-name').html('<input class = "edit-song-name" value="' + songName + '"></input>');
+
+//   var artistName = $songslist.find ('span.song-artistName'). text();
+//   $songslist.find('span.song-artistname').html('<input class = "edit-song-artistname" value="' + song-artistName + '"></input>');  
+
+//   var releaseDate = $songslist.find ('span.song-releaseDate'). text();
+//   $songslist.find('span.song-releaseDate').html('<input class = "edit-song-releaseDate" value="' + song-releaseDate + '"></input>');
+ 
+// //after editing songs when the save button is clicked
+
+// function handleSaveChangesClick(e) {
+//  var songId = $(this). closest('.song').data('song-id');
+//  var $songslist = $('[data-song-id=' +songId +']');
+
+//  var data = {
+//    name: $songslist.find('edit-song-name'). val(),
+//    artistname:$songslist.find('edit-song-artistName'). val(),
+//    releasename:$songslist.find('edit-song-releaseDate'). val(),
+//   };
+
+//   console.log('Updating song data', songId, 'with data', data);
+
+//   $.ajax ({
+//     method: 'PUT',
+//     url:'api/genre/' + genre + '/songs',
+//     success: handleSongUpdatedResponse
+//   });
+//  }
+
+
+//function handleSongUpdatedResponse(data) {
+  //console.log('response to update', data);
+  //var SongId = data._id;
+  //$('[data-song-id=' +songId +']').remove();
+  //renderSong(data);
+
+//function handleDeleteSongClick(e) {
+  //var SongId = $(this).parents('song') .data('song-id');
+
+
+//$.ajax ({
+  //method: 'DELETE',
+  //url:'api/genre/' + genre + '/songs'
+  //success: handleDeleteSongSuccess
+ //});
+//}
+
+//function handleDeleteSongSucess(data) {
+  //var deletedSongId = data._id;
+  //console.log('you have deleted this song');
+  //$('[data-song-id=' +songId +']'). remove();
+
+//}
+
+//function renderSong (song)  {
+  //console.log('rendering song', song);
+
+//}
+
+
+
+
+//})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
