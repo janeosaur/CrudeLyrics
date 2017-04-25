314e7886776b2bdf991d98deb7172270b7dbffcb
janeosaur <janewie@gmail.com>
2017-04-25 00:20:43 -0700

    heroku deploy attempt#2

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost/crudelyrics");
-mongoose.connect( process.env.MONGODB_URI || "localhost:3000/" );
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/crudelyrics" );
 
 module.exports.Song = require("./songs.js");
 module.exports.Lyric = require("./lyrics.js");
